aca15b2798db2d276e879b0ce50cc6365d09cd10
bu-sy <hidden@email.com>
2025-12-14 15:45:17 +0100

    Refactored + added from_playlist field in records, so it can be traced

--- code/spotify_reader.py
+++ code/spotify_reader.py
@@ -2,31 +2,33 @@ import os
 import requests
 import yaml
 
-# TODO add authorization flow
-# assert os.environ.get('SPOTIFY_CLIENT_ID') and os.environ.get('SPOTIFY_CLIENT_SECRET')
+
+class SpotifyCaller(object):
+    def __init__(self, token_id):# TODO add authorization flow with client_id and client_secret
+        self.request_headers = {
+            'Authorization': f'Bearer {token_id}'
+        }
+
+    def get_tracks(self, playlist_id):
+        response = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=self.request_headers)
+        response.raise_for_status()
+        result = response.json()
+        all_tracks = []
+        for item_in_the_list in result['items']:
+            track = item_in_the_list['track']
+            all_tracks.append({
+                'artist': ', '.join(sorted([artist['name'] for artist in track['artists']])),
+                'track_name': track['name'],
+                'links': [{
+                    'portal': 'Spotify',
+                    'link': track['external_urls']['spotify']
+                }],
+                'from_playlist': [f"https://open.spotify.com/playlist/{playlist_id}"]
+            })
+        return all_tracks
+
 
 token_id = os.environ.get('SPOTIFY_TOKEN_ID')
 playlist_id = os.environ.get('SPOTIFY_PLAYLIST_ID')
 
-request_headers = {
-    'Authorization': f'Bearer {token_id}',
-}
-
-response = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=request_headers)
-response.raise_for_status()
-
-result = response.json()
-all_tracks = []
-
-for item_in_the_list in result['items']:
-    track = item_in_the_list['track']
-    all_tracks.append({
-        'artist': ', '.join(sorted([artist['name'] for artist in track['artists']])),
-        'track_name': track['name'],
-        'links': [{
-            'portal': 'Spotify',
-            'link': track['external_urls']['spotify']
-        }]
-    })
-
-print(yaml.dump(all_tracks, allow_unicode=True))
+print(yaml.dump(SpotifyCaller(token_id=os.environ.get('SPOTIFY_TOKEN_ID')).get_tracks(os.environ.get('SPOTIFY_PLAYLIST_ID')), allow_unicode=True))

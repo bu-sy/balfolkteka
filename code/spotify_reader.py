9053aa2451adfdc39c673ee7e2726304d3061dd6
bu-sy <hidden@email.com>
2025-12-15 09:01:32 +0100

    Updated Spotify reader, to allow manual downloads of playlists for the users. Created backlog of playlists to distribute between appropriate dances

--- code/spotify_reader.py
+++ code/spotify_reader.py
@@ -29,8 +29,21 @@ class SpotifyCaller(object):
             })
         return all_tracks
 
+    def get_playlists(self, user_id):
+        response = requests.get(f"https://api.spotify.com/v1/users/{user_id}/playlists", headers=self.request_headers)
+        response.raise_for_status()
+        result = response.json()
+        return [{
+            'name': item['name'],
+            'link': item['external_urls']['spotify']
+        } for item in result['items']]
+
 
 token_id = os.environ.get('SPOTIFY_TOKEN_ID')
 playlist_id = os.environ.get('SPOTIFY_PLAYLIST_ID')
+user_id = os.environ.get('SPOTIFY_USER_ID')
 
-print(yaml.dump(SpotifyCaller(token_id=os.environ.get('SPOTIFY_TOKEN_ID')).get_tracks(os.environ.get('SPOTIFY_PLAYLIST_ID')), allow_unicode=True))
+if playlist_id:
+    print(yaml.dump(SpotifyCaller(token_id=token_id).get_tracks(playlist_id), allow_unicode=True))
+elif user_id:
+    print(yaml.dump(SpotifyCaller(token_id=token_id).get_playlists(user_id), allow_unicode=True))

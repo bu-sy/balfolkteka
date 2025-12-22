7a25532fe92bea46ca8de8a9912a0384f43f294d
bu-sy <hidden@email.com>
2025-12-22 07:56:42 +0100

    Added support for embedding bandtrack links

--- code/renderer.py
+++ code/renderer.py
@@ -24,6 +24,9 @@ def embed_youtube(link_text):
 def embed_soundcloud(link_text):
     return '''<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="''' + link_text + '''&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" loading="lazy"></iframe>'''
 
+def embed_bandcamp(link_text):
+    return '''<iframe style="border: 0; width: 100%; height: 42px;" src="''' + link_text + '''" seamless></iframe>'''
+
 def collapsible(summary, contents):
     return f"<details>\n<summary><big>{summary}</big></summary>\n{contents}\n</details>"
 
@@ -34,6 +37,8 @@ def embed_track(link_object):
         return embed_youtube(link_object.link)
     elif link_object.portal.lower() == 'soundcloud':
         return embed_soundcloud(link_object.link)
+    elif link_object.portal.lower() == 'bandcamp':
+        return embed_bandcamp(link_object.link)
     else:
         return f"({link(link_object.portal, link_object.link)})"
 
--- documentation/pl/dances/scottische.md
+++ documentation/pl/dances/scottische.md
@@ -333,6 +333,8 @@
 <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5uRbx0kPaCb3zPRJ2gTotc?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
 <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/soundcloud:tracks:469767066&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" loading="lazy"></iframe>
+
+<iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=1025616882/size=small/bgcol=ffffff/linkcol=0687f5/track=3959144873/transparent=true/" seamless></iframe>
 </details>
 
 <details>
